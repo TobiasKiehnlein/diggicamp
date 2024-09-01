fa0b13a8cf4cdc8e21218951e7b04d2e77f3b84c
1337src <180120548+1337src@users.noreply.github.com>
2024-09-01 23:52:42 +0200

    feat: updated the login routine (shibboleth stuff)
    updated the login because digicampus now has a new startpage and uses shibboleth or so for login. now it works with the current version of digicampus

--- diggicamp/scraper.py
+++ diggicamp/scraper.py
@@ -2,10 +2,12 @@ import os
 import requests
 import urllib
 from getpass import getpass
+from urllib.parse import quote
+from bs4 import BeautifulSoup
 
 from .config import DiggicampConf
 from .exceptions import WebException, NotLoggedInScepion
-from .pages import login, courses, course_files
+from .pages import courses, course_files
 
 
 class Diggicamp:
@@ -26,8 +28,6 @@ class Diggicamp:
         if self.verbose:
             print("authed: {}".format(self.authed))
 
-        html = self._get('/?sso=webauth&cancel_login=1&again=yes', unauthed=True)
-
         if not self.conf.has('credentials'):
             raise Exception("No credentials in config!")
 
@@ -41,18 +41,10 @@ class Diggicamp:
         else:
             raise Exception("Unknown auth mode: " + self.conf.get('credentials.mode'))
 
-        # parse page and get form data
-        page = login.LoginPage(html)
-
-        text = self._post(page.url(), data=page.assembleFormData(user, pw),
-                          base='https://websso.uni-augsburg.de/', unauthed=True)
-
-        redirect = page.getRedirectUrlFromResponse(text)
-
-        self._get(redirect, base='', unauthed=True)
-
+        self.session = self.login_and_get_session(user, pw)
         self.authed = True
         self._save_cookies()
+        print(f"Logged in as {user} and saved cookies")
 
     def get_courses(self, cached: bool = True):
         if cached and self.conf.has('courses'):
@@ -181,7 +173,64 @@ class Diggicamp:
         """
         self.session.cookies.clear()
         self.authed = False
+    
+    def login_and_get_session(self, username, password):
+        # Define the URLs
+        digicampus_url = "https://digicampus.uni-augsburg.de"
 
+        # Create a session object to persist cookies across requests and set the user agent
+        session = requests.Session()
+        session.headers.update({"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"})
+
+        # First make request to digicampus url
+        response = session.get(digicampus_url)
+
+        # find the login URL in the response
+        soup = BeautifulSoup(response.text, "html.parser")
+        # find all list items with the class "login_link"
+        login_items = soup.find_all("li", class_="login_link")
+        
+        login_url = None
+        for item in login_items:
+            # check if the text of the a tag contains "Login"
+            if "Login" in item.a.text:
+                if "RZ-Kennung" in item.a.text:
+                    login_url = item.a.get("href")
+                    break
+
+        if not login_url:
+            print("Login URL not found. Stopping login process.")
+            return
+
+        # "click" on the Login link to get the login page
+        response = session.get(login_url)
+
+        # Add username and password and make the login request
+        url_with_credentials = response.url + "&username=" + quote(username) + "&password=" + quote(password)
+
+        # Perform the login request
+        response = session.get(url_with_credentials, allow_redirects=True)
+
+        # find the form in the response and send the login request
+        soup = BeautifulSoup(response.text, "html.parser")
+        form = soup.find("form")
+        response = session.post(form.get("action"), data={"username": username, "password": password})
+
+        # find the form in the response and extract the SAMLResponse, form action and method
+        soup = BeautifulSoup(response.text, "html.parser")
+        form = soup.find("form")
+        form_action = form.get("action")
+        form_method = form.get("method")
+        SAML_Response = form.find("input", {"name": "SAMLResponse"}).get("value")
+
+        # make the request 
+        session.request(method=form_method, url=form_action, data={"SAMLResponse": SAML_Response})
+
+        # make two last requests to get the final cookies
+        session.get("https://digicampus.uni-augsburg.de/?sso=shibboleth&cancel_login=1&again=yes")
+        session.get("https://digicampus.uni-augsburg.de/?sso=multiaccount&cancel_login=1&again=1")
+
+        return session
 
 def is_not_logged_in(resp: requests.Response) -> bool:
     # user is not logged in, if we get a html page back which contains the following text
